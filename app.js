243a6c592997446e81c6695d220361fbb71a38ed
Essa <essa.ea68@gmail.com>
2024-01-11 20:00:21 +1100

    teamable-app-test

---
+++ app.js
@@ -0,0 +1,28 @@
+
+function handleEditProfile() {
+
+        
+
+       document.getElementById("edit-view").style.display = "block"
+
+       document.getElementById("display-view").style.display = "none"
+
+
+}
+
+function handleUpdateProfile() {
+
+      /*var updatedName =  document.getElementById("input-name").value
+      var name =  document.getElementById("name")
+      name.textcontent = updatedName
+      
+
+      var updatedName =  document.getElementById("input-email").value
+      var email =  document.getElementById("email")
+      name.textcontent = updatedEmail */
+
+        document.getElementById("edit-view").style.display = "none"
+
+        document.getElementById("display-view").style.display = "block" 
+
+}
